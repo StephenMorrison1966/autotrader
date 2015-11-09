1c7d5a9bb230352eacdf99bd8868365c3c81986a
Stephen Morrison <stephen.morrison@careerbuilder.com>
2015-11-09 17:26:56 -0600

    After migrating Vehicle addition

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103003409) do
+ActiveRecord::Schema.define(version: 20151109231510) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 20151103003409) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "logo"
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string   "color"
+    t.string   "condition"
+    t.float    "price"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
