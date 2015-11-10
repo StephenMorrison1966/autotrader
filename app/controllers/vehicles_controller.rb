9f7bef43a015cac9ed25e0cd62782d47e5d59270
Stephen Morrison <stephen.morrison@careerbuilder.com>
2015-11-09 19:05:06 -0600

    After creating VehicleFeature associations and adding display changes

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -69,6 +69,10 @@ class VehiclesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def vehicle_params
-      params.require(:vehicle).permit(:color, :condition, :price, :category_id)
+      params.require(:vehicle).permit(:color, 
+                                      :condition, 
+                                      :price, 
+                                      :category_id,
+                                      {feature_ids:[]})
     end
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,8 @@
 class Vehicle < ActiveRecord::Base
 	belongs_to :category
+	has_many :vehicle_features
+	has_many :features, through: :vehicle_features
+
 
 	validates_presence_of :color, :condition, :price, :category
 end
